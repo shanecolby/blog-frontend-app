e16c8033492966e4eb56c719d806492d38010c81
shane <shanecolbyzero8@gmail.com>
2020-12-15 21:02:44 -0600

    add to postsnew

--- src/views/PostsNew.vue
+++ src/views/PostsNew.vue
@@ -1,8 +1,33 @@
 <template>
+
   <div class="posts-new">
     <form v-on:submit.prevent="submit()">
-    <h1>Make a new post</h1>
-    </div>
+      <h1>Make a new post</h1>
+      <ul>
+        <li class="text-danger" v-for="error in errors">{{ error }}</li>
+      </ul>
+      <div class="form-group">
+        <label>Title:</label> 
+        <input type="text" class="form-control" v-model="title">
+      </div>
+      <div class="form-group">
+        <label>Ingredients:</label>
+        <input type="text" class="form-control" v-model="ingredients">
+      </div>
+      <div class="form-group">
+        <label>Directions:</label>
+        <input type="text" class="form-control" v-model="directions">
+      </div>
+      <div class="form-group">
+        <label>Prep Time:</label>
+        <input type="text" class="form-control" v-model="prepTime">
+      </div>
+      <div class="form-group">
+        <label>Image Url:</label>
+        <input type="text" class="form-control" v-model="imageUrl">
+      </div>
+      <input type="submit" class="btn btn-primary" value="Submit">
+    </form>
   </div>
 </template>
 
