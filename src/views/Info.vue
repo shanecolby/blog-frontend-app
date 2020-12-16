b04410e3926005ebace078e79c2e6a38b0ecb53b
shane <shanecolbyzero8@gmail.com>
2020-12-15 19:02:18 -0600

    add info page

---
+++ src/views/Info.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="info">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "This is the INFO page!",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
